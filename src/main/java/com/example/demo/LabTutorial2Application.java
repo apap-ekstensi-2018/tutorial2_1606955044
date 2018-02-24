144d2141f4ae348fed8030b6f48c9bb8abe8a2f5
Shinta Kusuma Wardhani <shintawardhani@shintas.local>
2018-02-24 15:08:30 +0700

    create file tutorial2

---
+++ src/main/java/com/example/demo/LabTutorial2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTutorial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTutorial2Application.class, args);
+	}
+}
